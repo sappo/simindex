aa5a3720c629d1aea6566b9d07e26c94788b6914
Kevin Sapper <mail@kevinsapper.de>
2017-02-13 13:42:33 +0100

    More efficient tf/idf similarity normalization

--- simindex/weak_labels.py
+++ simindex/weak_labels.py
@@ -75,7 +75,7 @@ class WeakLabels(object):
     def tfidf_similarity(self, t1, t2):
         """
         Calculates the TF/IDF similarity between two terms `t1` and `t2` taking
-        a dictionary and TfidfModel into account.
+        a dictionary and TfidfModel into account. (WHIRL)
 
         Returns similarity value between 0 and 1.
         """
@@ -88,21 +88,16 @@ class WeakLabels(object):
         tfidf_1 = dict(self.tfidf_model[bow_t1])
         tfidf_2 = dict(self.tfidf_model[bow_t2])
 
-        # Normalize weights
+        # Normalized vector of tfidf weights
         tfidf_1_norm = math.sqrt(sum([math.pow(w, 2) for w in tfidf_1.values()]))
-        for q in tfidf_1.keys():
-            tfidf_1[q] = tfidf_1[q] / tfidf_1_norm
-
         tfidf_2_norm = math.sqrt(sum([math.pow(w, 2) for w in tfidf_2.values()]))
-        for q in tfidf_2.keys():
-            tfidf_2[q] = tfidf_2[q] / tfidf_2_norm
 
         # Calculate similarity
         for w1_id in tfidf_1.keys():
             if w1_id in tfidf_2:
                 similarity += tfidf_1[w1_id] * tfidf_2[w1_id]
 
-        return similarity
+        return similarity / (tfidf_1_norm * tfidf_2_norm)
 
     def predict(self):
         blocker = {}
