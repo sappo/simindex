* Indexer method to generate blocks with candidate ids for blocking   scheme learner. * Fixed fit similarity calculation by only calculating attribute   with overlapping bkvs

simindex/engine.py
@@ -174,6 +174,7 @@ class SimEngine(object):
                 blocking_keys.append(BlockingKey(field, term_id))
 
             dbs = DisjunctiveBlockingScheme(blocking_keys, P, N,
+                                            self.indexer_class,
                                             self.max_bk_conjunction,
                                             verbose=self.verbose)
             self.blocking_scheme = dbs.transform(dataset)
@@ -212,9 +213,43 @@ class SimEngine(object):
             for measure in SimLearner.strings_to_prediction(self.similarities):
                 similarity_fns.append(measure().compare)
 
-            fields = set()
-            for blocking_key in self.blocking_scheme:
-                fields.update(blocking_key.covered_fields())
+            X_P = []
+            y_P = []
+            for pair in P:
+                x = np.zeros(self.attribute_count, np.float)
+                for blocking_key in self.blocking_scheme:
+                    p1_attributes = dataset[pair.t1]
+                    p2_attributes = dataset[pair.t2]
+                    p1_bkvs = blocking_key.blocking_key_values(p1_attributes)
+                    p2_bkvs = blocking_key.blocking_key_values(p2_attributes)
+                    if not p1_bkvs.isdisjoint(p2_bkvs):
+                        for field in blocking_key.covered_fields():
+                            p1_attribute = p1_attributes[field]
+                            p2_attribute = p2_attributes[field]
+                            if p1_attribute and p2_attribute:
+                                x[field] = similarity_fns[field](p1_attribute, p2_attribute)
+
+                X_P.append(x)
+                y_P.append(1)
+
+            X_N = []
+            y_N = []
+            for pair in N:
+                x = np.zeros(self.attribute_count, np.float)
+                for blocking_key in self.blocking_scheme:
+                    p1_attributes = dataset[pair.t1]
+                    p2_attributes = dataset[pair.t2]
+                    p1_bkvs = blocking_key.blocking_key_values(p1_attributes)
+                    p2_bkvs = blocking_key.blocking_key_values(p2_attributes)
+                    if not p1_bkvs.isdisjoint(p2_bkvs):
+                        for field in blocking_key.covered_fields():
+                            p1_attribute = p1_attributes[field]
+                            p2_attribute = p2_attributes[field]
+                            if p1_attribute and p2_attribute:
+                                x[field] = similarity_fns[field](p1_attribute, p2_attribute)
+
+                X_N.append(x)
+                y_N.append(0)
 
             tuned_parameters = [
                     {'kernel': ['linear'],  'C': [0.1, 1, 10, 100, 1000]},
@@ -225,32 +260,6 @@ class SimEngine(object):
             self.clf = skm.model_selection.GridSearchCV(
                     skm.svm.SVC(C=1, class_weight='balanced'),
                     tuned_parameters, scoring='f1_macro', refit=False)
-            X_P = []
-            y_P = []
-            for pair in P:
-                x = np.zeros(self.attribute_count, np.float)
-                p1_attributes = dataset[pair.t1]
-                p2_attributes = dataset[pair.t2]
-                for field, (p1_attribute, p2_attribute) in enumerate(zip(p1_attributes, p2_attributes)):
-                    if field in fields and p1_attribute and p2_attribute:
-                        x[field] = similarity_fns[field](p1_attribute, p2_attribute)
-
-                X_P.append(x)
-                y_P.append(1)
-
-            X_N = []
-            y_N = []
-            for pair in N:
-                x = np.zeros(self.attribute_count, np.float)
-                p1_attributes = dataset[pair.t1]
-                p2_attributes = dataset[pair.t2]
-                for field, (p1_attribute, p2_attribute) in enumerate(zip(p1_attributes, p2_attributes)):
-                    if field in fields and p1_attribute and p2_attribute:
-                        x[field] = similarity_fns[field](p1_attribute, p2_attribute)
-
-                X_N.append(x)
-                y_N.append(0)
-
             # Shrink training set to max 5000 samples
             # P
             X_train = []

simindex/weak_labels.py
@@ -317,7 +317,7 @@ class Feature:
 
 class DisjunctiveBlockingScheme(object):
 
-    def __init__(self, blocking_keys, P, N, k=2,
+    def __init__(self, blocking_keys, P, N, indexer, k=2,
                  block_timer=None, verbose=False):
         self.P = [frozenset((p.t1, p.t2)) for p in P]
         self.N = [frozenset((p.t1, p.t2)) for p in N]
@@ -325,6 +325,7 @@ class DisjunctiveBlockingScheme(object):
         self.flat_P = set(hp.flatten(self.frozen_P))
         self.features = []
         self.k = k
+        self.indexer = indexer
 
         self.verbose = verbose
         self.block_timer = block_timer
@@ -379,26 +380,13 @@ class DisjunctiveBlockingScheme(object):
             logger.info("%r" % feature.signature())
             logger.info("0) Build blocks\t\t\t%s" % hp.memory_usage())
 
-        FBI = defaultdict(dict)
-        for r_id, r_attributes in self.dataset.items():
-            blocking_key_values = feature.blocking_key_values(r_attributes)
-            for encoding in blocking_key_values:
-                for blocking_key in feature.blocking_keys:
-                    field = blocking_key.field
-
-                    BI = FBI[field]
-                    if encoding not in BI.keys():
-                        BI[encoding] = set()
-
-                    BI[encoding].add(r_id)
+        blocks = self.indexer.blocks(feature, self.dataset)
 
         # Check frequency distribution of blocks
         block_sizes = []
-        nblocks = 0
-        for BI in FBI.values():
-            nblocks += len(BI)
-            for block_key in BI.keys():
-                block_sizes.append(len(BI[block_key]))
+        nblocks = len(blocks)
+        for block_key, block in blocks:
+            block_sizes.append(len(block))
 
         freq_dis = Counter(block_sizes)
         ngood = sum([freq_dis[b] * b for b in freq_dis.keys() if b > 1 and b < 101])
@@ -418,40 +406,39 @@ class DisjunctiveBlockingScheme(object):
             logger.info("----------------------------------------------------")
             return 0
 
-        FBI_candidate_pairs = set()
+        term_candidate_pairs = set()
         TP, FP, FN = 0, 0, 0
-        for BI in FBI.values():
-            for block_key in BI.keys():
-                if len(BI[block_key]) > 100:
-                    feature.add_illegal_bkv(block_key)
-                    continue
+        for block_key, block in blocks:
+            if len(block) > 100:
+                feature.add_illegal_bkv(block_key)
+                continue
 
-                candidates = BI[block_key]
-                # Generate candidate pairs
-                cp_list = it.combinations(candidates, 2)
-                candidate_pairs = set([frozenset(p) for p in cp_list])
-                del cp_list
+            candidates = block
+            # Generate candidate pairs
+            cp_list = it.combinations(candidates, 2)
+            block_candidate_pairs = set([frozenset(p) for p in cp_list])
+            del cp_list
 
-                # Calculate TP, FP, FN
-                block_TP_pairs = candidate_pairs.intersection(self.frozen_P)
-                block_FP_pairs = candidate_pairs.difference(block_TP_pairs)
-                FP_canidates = set(hp.flatten(block_FP_pairs))
+            # Calculate TP, FP, FN
+            block_TP_pairs = block_candidate_pairs.intersection(self.frozen_P)
+            block_FP_pairs = block_candidate_pairs.difference(block_TP_pairs)
+            FP_canidates = set(hp.flatten(block_FP_pairs))
 
-                TP += len(block_TP_pairs)
-                FP += len(block_FP_pairs)
-                FN += len(FP_canidates.intersection(self.flat_P))
+            TP += len(block_TP_pairs)
+            FP += len(block_FP_pairs)
+            FN += len(FP_canidates.intersection(self.flat_P))
 
-                FBI_candidate_pairs.update(candidate_pairs)
+            term_candidate_pairs.update(block_candidate_pairs)
 
         if self.verbose:
-            logger.info("2) Generated candidates %s" % len(FBI_candidate_pairs))
+            logger.info("2) Generated candidates %s" % len(term_candidate_pairs))
             logger.info("3) Building ytrue/ypred\t\t%s" % hp.memory_usage())
 
         # Create feature vectors based on positive and negative labels
         y_true = np.zeros(len(self.P) + len(self.N), np.bool)
         y_pred = np.zeros(len(self.P) + len(self.N), np.bool)
         for index, pair in enumerate(self.P):
-            if pair in FBI_candidate_pairs:
+            if pair in term_candidate_pairs:
                 y_true[index] = True
                 y_pred[index] = True
             else:
@@ -459,7 +446,7 @@ class DisjunctiveBlockingScheme(object):
                 y_pred[index] = False
 
         for index, pair in enumerate(self.N):
-            if pair in FBI_candidate_pairs:
+            if pair in term_candidate_pairs:
                 y_true[index + len(self.P)] = False
                 y_pred[index + len(self.P)] = True
             else:
@@ -485,8 +472,8 @@ class DisjunctiveBlockingScheme(object):
         f1_score = 2 * ((precision * recall) / (precision + recall))
 
         # Force cleanup
-        del FBI
-        del FBI_candidate_pairs
+        del blocks
+        del term_candidate_pairs
 
         if self.verbose:
             logger.info("5) Calculated block metrics\t%s" % hp.memory_usage())