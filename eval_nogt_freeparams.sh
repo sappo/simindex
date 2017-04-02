21db2402022712a2e03d6e4e27456ee45b64b7b8
Kevin Sapper <mail@kevinsapper.de>
2017-04-02 20:57:15 +0200

    Add max_p and max_n to evaluation of free params

--- eval_nogt_freeparams.sh
+++ eval_nogt_freeparams.sh
@@ -7,12 +7,14 @@
 # Thresholds
 UPPER=$1
 WINDOW=$2
-CLF=$3
+MAX_P=$3
+MAX_N=$4
+CLF=$5
 for i in $(seq 1 10);
 do
     LOWER=`echo - | awk "{print $i / 10}"`
     if [ `echo - | awk "{print $LOWER <= $UPPER}"` -ne 0 ]; then
         rm -f ./.engine/.ncvoter*
-        EVAL_FLAGS="$CLF --no-classifier --no-gt-labels --gt-thresholds $LOWER $UPPER $WINDOW 0.1 0.25" ./analyze.sh -n
+        EVAL_FLAGS="$CLF --no-classifier --no-gt-labels --gt-thresholds $LOWER $UPPER $WINDOW $MAX_P $MAX_N" ./analyze.sh -n
     fi
 done
