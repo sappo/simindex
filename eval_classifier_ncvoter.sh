4eb454184f87a6f346419436da4c28777ace39a1
Kevin Sapper <mail@kevinsapper.de>
2017-04-13 11:40:55 +0200

    Better classifier testing

--- eval_classifier_ncvoter.sh
+++ eval_classifier_ncvoter.sh
@@ -7,14 +7,17 @@
 # Cleanup
 rm -f ./.engine/.ncvoter*
 
+# Evaluation with SVM decision tree
+EVAL_FLAGS="--clf=decisiontree --gt-labels" ./analyze.sh -n > /its/ksapp002/nohup.log
+EVAL_FLAGS="--clf=decisiontree --gt-labels --parfull-simvector" ./analyze.sh -n > /its/ksapp002/nohup.log
+EVAL_FLAGS="--clf=decisiontree --gt-labels --full-simvector" ./analyze.sh -n > /its/ksapp002/nohup.log
+
 # Evaluation with SVM linear
 EVAL_FLAGS="--clf=svmlinear --gt-labels" ./analyze.sh -n > /its/ksapp002/nohup.log
+EVAL_FLAGS="--clf=svmlinear --gt-labels --parfull-simvector" ./analyze.sh -n > /its/ksapp002/nohup.log
 EVAL_FLAGS="--clf=svmlinear --gt-labels --full-simvector" ./analyze.sh -n > /its/ksapp002/nohup.log
 
 # Evaluation with SVM rbf
 EVAL_FLAGS="--clf=svmrbf --gt-labels" ./analyze.sh -n > /its/ksapp002/nohup.log
+EVAL_FLAGS="--clf=svmrbf --gt-labels --parfull-simvector" ./analyze.sh -n > /its/ksapp002/nohup.log
 EVAL_FLAGS="--clf=svmrbf --gt-labels --full-simvector" ./analyze.sh -n > /its/ksapp002/nohup.log
-
-# Evaluation with SVM decision tree
-EVAL_FLAGS="--clf=decisiontree --gt-labels" ./analyze.sh -n > /its/ksapp002/nohup.log
-EVAL_FLAGS="--clf=decisiontree --gt-labels --full-simvector" ./analyze.sh -n > /its/ksapp002/nohup.log
