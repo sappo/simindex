d02690d86f52a32baa1aefc76598b3ee8dc73211
Kevin Sapper <mail@kevinsapper.de>
2017-04-16 00:10:21 +0200

    Use different fit / query datasets

--- analyze.sh
+++ analyze.sh
@@ -3,6 +3,13 @@ timestamp=$(date +%Y%m%d-%H%M%S)
 reportprefix="./reports"
 mkdir -p $reportprefix
 
+fit="train"
+query="train"
+if [ "$3" != '' ]; then
+    fit=$3
+    query=$4
+fi
+
 if [ "$1" == '-f' ]; then
 # Run ferbl datasets
 #for dataset in "ferbl-4k-1k-1" "ferbl-9k-1k-1" "ferbl-90k-10k-1"
@@ -21,7 +28,7 @@ do
             mprof run analyzer.py \
                 --run-type fit -r "${timestamp}" \
                 -o "${reportprefix}/${timestamp}_fit_${dataset}" \
-                -s ${datasetprefix}${dataset}_train_gold.csv -g id_1 -g id_2 \
+                -s ${datasetprefix}${dataset}_${fit}_gold.csv -g id_1 -g id_2 \
                 -t rec_id -t date_of_birth -t given_name -t surname -t state -t suburb -t postcode -t address_1 -t phone_number \
                 -b "0 surname term_id" \
                 -b "0 address_1 tokens" \
@@ -32,8 +39,8 @@ do
                 -m ${indexer} \
                 $EVAL_FLAGS \
                 ${datasetprefix}${dataset}_index.csv \
-                ${datasetprefix}${dataset}_train_query.csv \
-                ${datasetprefix}${dataset}_train.csv
+                ${datasetprefix}${dataset}_${query}_query.csv \
+                ${datasetprefix}${dataset}_${fit}.csv
             ) || exit 1
         else
             # Fit model once!
@@ -41,13 +48,13 @@ do
             mprof run analyzer.py \
                 --run-type fit -r "${timestamp}" \
                 -o "${reportprefix}/${timestamp}_fit_${dataset}" \
-                -s ${datasetprefix}${dataset}_train_gold.csv -g id_1 -g id_2 \
+                -s ${datasetprefix}${dataset}_${fit}_gold.csv -g id_1 -g id_2 \
                 -t rec_id -t date_of_birth -t given_name -t surname -t state -t suburb -t postcode -t address_1 -t phone_number \
                 -m ${indexer} \
                 $EVAL_FLAGS \
                 ${datasetprefix}${dataset}_index.csv \
-                ${datasetprefix}${dataset}_train_query.csv \
-                ${datasetprefix}${dataset}_train.csv
+                ${datasetprefix}${dataset}_${query}_query.csv \
+                ${datasetprefix}${dataset}_${fit}.csv
             ) || exit 1
         fi
 
@@ -59,12 +66,12 @@ do
             -i rec_id -i given_name -i surname -i suburb -i state -i postcode -i address_1 -i date_of_birth -i phone_number \
             -q rec_id -q given_name -q surname -q suburb -q state -q postcode -q address_1 -q date_of_birth -q phone_number \
             -t rec_id -t given_name -t surname -t suburb -t state -t postcode -t address_1 -t date_of_birth -t phone_number \
-            -s ${datasetprefix}${dataset}_train_gold.csv -g id_1 -g id_2 \
+            -s ${datasetprefix}${dataset}_${query}_gold.csv -g id_1 -g id_2 \
             --run-type build -r "${timestamp}" -m ${indexer} \
             $EVAL_FLAGS \
             ${datasetprefix}${dataset}_index.csv \
-            ${datasetprefix}${dataset}_train_query.csv \
-            ${datasetprefix}${dataset}_train.csv
+            ${datasetprefix}${dataset}_${query}_query.csv \
+            ${datasetprefix}${dataset}_${fit}.csv
         ) || exit 1
 
         # Calculate metrics on dataset
@@ -73,13 +80,13 @@ do
             -i rec_id -i given_name -i surname -i suburb -i state -i postcode -i address_1 -i date_of_birth -i phone_number \
             -q rec_id -q given_name -q surname -q suburb -q state -q postcode -q address_1 -q date_of_birth -q phone_number \
             -t rec_id -t given_name -t surname -t suburb -t state -t postcode -t address_1 -t date_of_birth -t phone_number \
-            -s ${datasetprefix}${dataset}_train_gold.csv -g id_1 -g id_2 \
+            -s ${datasetprefix}${dataset}_${query}_gold.csv -g id_1 -g id_2 \
             --run-type evaluation -o $result_output \
             -m ${indexer} \
             $EVAL_FLAGS \
             ${datasetprefix}${dataset}_index.csv \
-            ${datasetprefix}${dataset}_train_query.csv \
-            ${datasetprefix}${dataset}_train.csv
+            ${datasetprefix}${dataset}_${query}_query.csv \
+            ${datasetprefix}${dataset}_${fit}.csv
         ) || exit 1
 
     done
@@ -97,7 +104,7 @@ do
         mprof run -T 1.0 analyzer.py \
             --run-type fit -r "${timestamp}" \
             -o "${reportprefix}/${timestamp}_fit_${dataset}" \
-            -s ${datasetprefix}${dataset}_train_gold.csv -g id_1 -g id_2 \
+            -s ${datasetprefix}${dataset}_${fit}_gold.csv -g id_1 -g id_2 \
             -t id -t first_name -t middle_name -t last_name -t street_address -t city -t state -t zip_code -t full_phone_num \
             -b "0 last_name term_id" \
             -b "0 street_address tokens" \
@@ -108,20 +115,20 @@ do
             $EVAL_FLAGS \
             -m ${indexer} \
             ${datasetprefix}${dataset}_index.csv \
-            ${datasetprefix}${dataset}_train_query.csv \
-            ${datasetprefix}${dataset}_train.csv
+            ${datasetprefix}${dataset}_${query}_query.csv \
+            ${datasetprefix}${dataset}_${fit}.csv
     else
         # Fit model once!
         mprof run -T 1.0 analyzer.py \
             --run-type fit -r "${timestamp}" \
             -o "${reportprefix}/${timestamp}_fit_${dataset}" \
-            -s ${datasetprefix}${dataset}_train_gold.csv -g id_1 -g id_2 \
+            -s ${datasetprefix}${dataset}_${fit}_gold.csv -g id_1 -g id_2 \
             -t id -t first_name -t middle_name -t last_name -t street_address -t city -t state -t zip_code -t full_phone_num \
             $EVAL_FLAGS \
         -m ${indexer} \
             ${datasetprefix}${dataset}_index.csv \
-            ${datasetprefix}${dataset}_train_query.csv \
-            ${datasetprefix}${dataset}_train.csv
+            ${datasetprefix}${dataset}_${query}_query.csv \
+            ${datasetprefix}${dataset}_${fit}.csv
     fi
 
     result_output="${reportprefix}/${timestamp}_${indexer}_${dataset}"
@@ -130,25 +137,25 @@ do
         -i id -i first_name -i middle_name -i last_name -i city -i state -i zip_code -i street_address -i full_phone_num \
         -q id -q first_name -q middle_name -q last_name -q city -q state -q zip_code -q street_address -q full_phone_num \
         -t id -t first_name -t middle_name -t last_name -t city -t state -t zip_code -t street_address -t full_phone_num \
-        -s ${datasetprefix}${dataset}_train_gold.csv -g id_1 -g id_2 \
+        -s ${datasetprefix}${dataset}_${query}_gold.csv -g id_1 -g id_2 \
         --run-type build -r "${timestamp}" -m ${indexer} \
         $EVAL_FLAGS \
         ${datasetprefix}${dataset}_index.csv \
-        ${datasetprefix}${dataset}_train_query.csv \
-        ${datasetprefix}${dataset}_train.csv
+        ${datasetprefix}${dataset}_${query}_query.csv \
+        ${datasetprefix}${dataset}_${fit}.csv
 
     # Calculate metrics on dataset
     python -W ignore analyzer.py \
         -i id -i first_name -i middle_name -i last_name -i city -i state -i zip_code -i street_address -i full_phone_num \
         -q id -q first_name -q middle_name -q last_name -q city -q state -q zip_code -q street_address -q full_phone_num \
         -t id -t first_name -t middle_name -t last_name -t city -t state -t zip_code -t street_address -t full_phone_num \
-        -s ${datasetprefix}${dataset}_train_gold.csv -g id_1 -g id_2 \
+        -s ${datasetprefix}${dataset}_${query}_gold.csv -g id_1 -g id_2 \
         --run-type evaluation -o $result_output \
         -m ${indexer} \
         $EVAL_FLAGS \
         ${datasetprefix}${dataset}_index.csv \
-        ${datasetprefix}${dataset}_train_query.csv \
-        ${datasetprefix}${dataset}_train.csv
+        ${datasetprefix}${dataset}_${query}_query.csv \
+        ${datasetprefix}${dataset}_${fit}.csv
 
 done
 elif [ "$1" == '-r' ]; then
@@ -164,7 +171,7 @@ do
         mprof run analyzer.py \
             --run-type fit -r "${timestamp}" \
             -o "${reportprefix}/${timestamp}_fit_${dataset}" \
-            -s ${datasetprefix}${dataset}_train_gold.csv -g id_1 -g id_2 \
+            -s ${datasetprefix}${dataset}_${fit}_gold.csv -g id_1 -g id_2 \
             -t id -t name -t addr -t city -t phone -t type \
             -b "0 name term_id" \
             -b "0 city tokens" \
@@ -172,20 +179,20 @@ do
             $EVAL_FLAGS \
             -m ${indexer} \
             ${datasetprefix}${dataset}_index.csv \
-            ${datasetprefix}${dataset}_train_query.csv \
-            ${datasetprefix}${dataset}_train.csv
+            ${datasetprefix}${dataset}_${query}_query.csv \
+            ${datasetprefix}${dataset}_${fit}.csv
     else
         # Fit model once!
         mprof run analyzer.py \
             --run-type fit -r "${timestamp}" \
             -o "${reportprefix}/${timestamp}_fit_${dataset}" \
-            -s ${datasetprefix}${dataset}_train_gold.csv -g id_1 -g id_2 \
+            -s ${datasetprefix}${dataset}_${fit}_gold.csv -g id_1 -g id_2 \
             -t id -t name -t addr -t city -t phone -t type \
             $EVAL_FLAGS \
             -m ${indexer} \
             ${datasetprefix}${dataset}_index.csv \
-            ${datasetprefix}${dataset}_train_query.csv \
-            ${datasetprefix}${dataset}_train.csv
+            ${datasetprefix}${dataset}_${query}_query.csv \
+            ${datasetprefix}${dataset}_${fit}.csv
     fi
 
     result_output="${reportprefix}/${timestamp}_${indexer}_${dataset}"
@@ -194,25 +201,25 @@ do
         -i id -i name -i addr -i city -i phone -i type \
         -q id -q name -q addr -q city -q phone -q type \
         -t id -t name -t addr -t city -t phone -t type \
-        -s ${datasetprefix}${dataset}_train_gold.csv -g id_1 -g id_2 \
+        -s ${datasetprefix}${dataset}_${query}_gold.csv -g id_1 -g id_2 \
         --run-type build -r "${timestamp}" -m ${indexer} \
         $EVAL_FLAGS \
         ${datasetprefix}${dataset}_index.csv \
-        ${datasetprefix}${dataset}_train_query.csv \
-        ${datasetprefix}${dataset}_train.csv
+        ${datasetprefix}${dataset}_${query}_query.csv \
+        ${datasetprefix}${dataset}_${fit}.csv
 
     # Calculate metrics on dataset
     python -W ignore analyzer.py \
         -i id -i name -i addr -i city -i phone -i type \
         -q id -q name -q addr -q city -q phone -q type \
         -t id -t name -t addr -t city -t phone -t type \
-        -s ${datasetprefix}${dataset}_train_gold.csv -g id_1 -g id_2 \
+        -s ${datasetprefix}${dataset}_${query}_gold.csv -g id_1 -g id_2 \
         --run-type evaluation -o $result_output \
         -m ${indexer} \
         $EVAL_FLAGS \
         ${datasetprefix}${dataset}_index.csv \
-        ${datasetprefix}${dataset}_train_query.csv \
-        ${datasetprefix}${dataset}_train.csv
+        ${datasetprefix}${dataset}_${query}_query.csv \
+        ${datasetprefix}${dataset}_${fit}.csv
 
 done
 fi
