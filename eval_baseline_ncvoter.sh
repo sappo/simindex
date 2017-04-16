2630835d0d605934af58bd4d3752e34aa485a075
Kevin Sapper <mail@kevinsapper.de>
2017-04-16 10:19:04 +0200

    More baseline eval control

--- eval_baseline_ncvoter.sh
+++ eval_baseline_ncvoter.sh
@@ -6,22 +6,31 @@
 
 # Baseline
 if [ "$1" == 'trtr' ]; then
-    rm -f ./.engine/.ncvoter*
-    EVAL_FLAGS="--gt-labels --parfull-simvector" ./analyze.sh -n -x train train &> /its/ksapp002/nohup.log
-    rm -f ./.engine/.ncvoter*
-    EVAL_FLAGS="--gt-labels --parfull-simvector" ./analyze.sh -n -b train train &> /its/ksapp002/nohup.log
+    if [ "$2" != '-b' ]; then
+        rm -f ./.engine/.ncvoter*
+        EVAL_FLAGS="--gt-labels --parfull-simvector" ./analyze.sh -n -x train train &> /its/ksapp002/nohup.log
+    else
+        rm -f ./.engine/.ncvoter*
+        EVAL_FLAGS="--gt-labels --parfull-simvector" ./analyze.sh -n -b train train &> /its/ksapp002/nohup.log
+    fi
 fi
 
 if [ "$1" == 'tete' ]; then
-    rm -f ./.engine/.ncvoter*
-    EVAL_FLAGS="--gt-labels --parfull-simvector" ./analyze.sh -n -x test test &> /its/ksapp002/nohup.log
-    rm -f ./.engine/.ncvoter*
-    EVAL_FLAGS="--gt-labels --parfull-simvector" ./analyze.sh -n -b test test &> /its/ksapp002/nohup.log
+    if [ "$2" != '-b' ]; then
+        rm -f ./.engine/.ncvoter*
+        EVAL_FLAGS="--gt-labels --parfull-simvector" ./analyze.sh -n -x test test &> /its/ksapp002/nohup.log
+    else
+        rm -f ./.engine/.ncvoter*
+        EVAL_FLAGS="--gt-labels --parfull-simvector" ./analyze.sh -n -b test test &> /its/ksapp002/nohup.log
+    fi
 fi
 
 if [ "$1" == 'teva' ]; then
-    rm -f ./.engine/.ncvoter*
-    EVAL_FLAGS="--gt-labels --parfull-simvector" ./analyze.sh -n -x test validate &> /its/ksapp002/nohup.log
-    rm -f ./.engine/.ncvoter*
-    EVAL_FLAGS="--gt-labels --parfull-simvector" ./analyze.sh -n -b test validate &> /its/ksapp002/nohup.log
+    if [ "$2" != '-b' ]; then
+        rm -f ./.engine/.ncvoter*
+        EVAL_FLAGS="--gt-labels --parfull-simvector" ./analyze.sh -n -x test validate &> /its/ksapp002/nohup.log
+    else
+        rm -f ./.engine/.ncvoter*
+        EVAL_FLAGS="--gt-labels --parfull-simvector" ./analyze.sh -n -b test validate &> /its/ksapp002/nohup.log
+    fi
 fi
